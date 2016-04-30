f2b6c8dd7aa28b13533309bc7d53a304c6a5932a
Ronan Connolly <hi@ronanconnolly.ie>
2016-04-30 02:30:35 +0100

    Added a second hidden layer. XOR now works. Added timings to readme.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # NeuralNetwork-Backpropagation
-A custom Neural Network program created in Java, using the backpropagation learning algorithm. Input, Hidden and Output layers and nodes can be scaled simply.
+A custom Neural Network program created in Java.
+Using the backpropagation learning algorithm. Input, Hidden and Output layers and nodes can be scaled simply.
+
+## Info
+Right not it is very basic, but with added enhancements timings could be improved.
+
+This book chapter was referenced:  
+https://web.archive.org/web/20150317210621/https://www4.rgu.ac.uk/files/chapter3%20-%20bp.pdf
+
+The calculations used come from this book.
+
+## Epoch calculations
+It took 291 epoch iterations for one entry:  
+**Final epoch iteration**  
+Epoch #291  
+Target Value:       1.000000  
+Output Node Value:  0.950049  
+Difference:     0.049951  
+
+When we have a batch of four entries per epoch it gives the following results for each set:  
+**AND** 
+54063  
+27708  
+403987   
+27691  
+451407  
+30653  
+
+**OR**  
+8111  
+5373  
+114112  
+31513  
+8524  
+8524  
+ 
+**XOR**  
+260432  
+NA  
+NA  
+10530815  
+NA  
+NA  
+811641  
--- src/ie/gmit/sw/Runner.java
+++ src/ie/gmit/sw/Runner.java
@@ -10,19 +10,24 @@ public class Runner {
 	private int outputNum = 1;
 	private double target = 1;
 	private double learningRate = 0.5;
-	double errorThreshold = 0.0001;
+	double errorThreshold = 0.001;
 	// 257113, 36825
 
 	// lists
 	ArrayList<Node> inputs = new ArrayList<Node>();
-	ArrayList<Node> hidden = new ArrayList<Node>();
+
+	ArrayList<ArrayList<Node>> hiddenLayers = new ArrayList<ArrayList<Node>>();
+	ArrayList<Node> hidden1 = new ArrayList<Node>();
+	ArrayList<Node> hidden2 = new ArrayList<Node>();
+
 	ArrayList<Node> outputs = new ArrayList<Node>();
 	ArrayList<Connection> inputConnections = new ArrayList<Connection>();
 	ArrayList<Connection> hiddenConnections = new ArrayList<Connection>();
 	ArrayList<TrainingSet> setAnd = new ArrayList<TrainingSet>();
 	ArrayList<TrainingSet> setOr = new ArrayList<TrainingSet>();
 	ArrayList<TrainingSet> setXor = new ArrayList<TrainingSet>();
-	
+	ArrayList<TrainingSet> chosenSet;
+
 	// misc
 	private Random random = new Random();
 	int nodeCount, connectionCount;
@@ -32,6 +37,9 @@ public class Runner {
 	double sumSquaredErrors;
 
 	public Runner() {
+		hiddenLayers.add(hidden1);
+		hiddenLayers.add(hidden2);
+
 		createNodes();
 		createConnections();
 
@@ -39,9 +47,10 @@ public class Runner {
 		genAndSet();
 		genOrSet();
 		genXorSet();
+		
+		chosenSet = setXor;
 
-//		displayAll();
-		// displayHiddenShort();
+		displayAll();
 
 		// epoch currently has one set
 		// a set consists of the input values and a target
@@ -49,16 +58,15 @@ public class Runner {
 		// target
 		do {
 			epochCount++;
-			for (TrainingSet trainingSet : setAnd) {
-				sumSquaredErrors = 0.0;
+			sumSquaredErrors = 0.0;
+			for (TrainingSet trainingSet : chosenSet) {
 				int count = 0;
 				for (Node node : inputs) {
 					node.setValue(trainingSet.getInputs()[count]);
 					count++;
 				}
 				target = trainingSet.getTarget();
-				
-				
+
 				//// forward pass
 				genAllValues();
 				// displayHiddenShort();
@@ -83,80 +91,130 @@ public class Runner {
 				// System.out.println("Updated Hidden Weights");
 				// displayHiddenShort();
 
-				
-				sumSquaredErrors += difference * difference;
-				displayOutput();
+				calcSumSquaredErrors();
+				// displayOutput();
+				// System.out.printf("\nEpoch #%d\n", epochCount);
+				// displayHiddenShort();
 			}
-			
-//			System.out.println("\ndifference: " + difference);
-//			System.out.println("errorThreshold: " + errorThreshold);
-//			System.out.println("difference > errorThreshold: " + (difference > errorThreshold));
+
+			// System.out.println("\ndifference: " + difference);
+			// System.out.println("errorThreshold: " + errorThreshold);
+			// System.out.println("difference > errorThreshold: " + (difference
+			// > errorThreshold));
 		} while (sumSquaredErrors > errorThreshold || sumSquaredErrors < -errorThreshold);
 
-		System.out.println();
-//		displayAll();
+		System.out.println("\n\n**FINISHED**");
+		displayAll();
 
-		System.out.println("**Final epoch iteration**");
-		displayOutput();
+		// System.out.println("**SHORT HIDDEN**");
+		// displayHiddenShort();
+
+		System.out.println("\n**Final epoch iteration**");
+		displayFinale();
+		
+		System.out.println("\n**Testing Chosen Set**");	// TODO: add name
+		testSet();
 
 		// an epoch may contain a set of items to test
 	}
-	
+
+	private void testSet() {
+		for (TrainingSet trainingSet : chosenSet) {
+			int count = 0;
+			for (Node node : inputs) {
+				node.setValue(trainingSet.getInputs()[count]);
+				count++;
+			}
+			target = trainingSet.getTarget();
+
+			//// forward pass
+			genAllValues();
+			
+			for (Node node : outputs) {
+				displayInputsShort();
+				System.out.printf("Output Node Value:\t%f\n", node.getValue());
+			}
+		}
+	}
+
 	private void genXorSet() {
-		setXor.add(new TrainingSet(1, 1, 0));
-		setXor.add(new TrainingSet(1, 0, 1));
-		setXor.add(new TrainingSet(0, 1, 1));
 		setXor.add(new TrainingSet(0, 0, 0));
+		setXor.add(new TrainingSet(0, 1, 1));
+		setXor.add(new TrainingSet(1, 0, 1));
+		setXor.add(new TrainingSet(1, 1, 0));
 	}
 
 	private void genOrSet() {
-		setOr.add(new TrainingSet(1, 1, 1));
-		setOr.add(new TrainingSet(1, 0, 1));
-		setOr.add(new TrainingSet(0, 1, 1));
 		setOr.add(new TrainingSet(0, 0, 0));
+		setOr.add(new TrainingSet(0, 1, 1));
+		setOr.add(new TrainingSet(1, 0, 1));
+		setOr.add(new TrainingSet(1, 1, 1));
 	}
 
 	private void genAndSet() {
 		// AND
-		setAnd.add(new TrainingSet(1, 1, 1));
-		setAnd.add(new TrainingSet(1, 0, 0));
-		setAnd.add(new TrainingSet(0, 1, 0));
 		setAnd.add(new TrainingSet(0, 0, 0));
+		setAnd.add(new TrainingSet(0, 1, 0));
+		setAnd.add(new TrainingSet(1, 0, 0));
+		setAnd.add(new TrainingSet(1, 1, 1));
+
+	}
+
+	private void calcSumSquaredErrors() {
+		sumSquaredErrors = 0.0;
+		for (Node node : outputs) {
+			difference = target - node.getValue();
+			sumSquaredErrors += difference * difference;
+		}
+//		System.out.println("sumSquaredErrors: " + sumSquaredErrors);
+	}
+
+	private void displayFinale() {
+		System.out.printf("Epoch #%d\n", epochCount);
+		for (Node node : outputs) {
+//			System.out.printf("Output Node Value:\t%f\n", node.getValue());
+		}
+		System.out.printf("sumSquaredErrors:\t%f\n", sumSquaredErrors);
 	}
 
 	private void displayOutput() {
 		for (Node node : outputs) {
-			difference = target - node.getValue();
 			System.out.printf("Epoch #%d\n", epochCount);
 			displayInputsShort();
-			System.out.printf("Target Value:\t\t%f\n", target);
+			// System.out.printf("Target Value:\t\t%f\n", target);
 			System.out.printf("Output Node Value:\t%f\n", node.getValue());
-//			System.out.printf("Difference:\t\t%f\n", difference);
-			System.out.printf("sumSquaredErrors:\t%f\n", sumSquaredErrors);
+			// System.out.printf("Difference:\t\t%f\n", difference);
 		}
+		System.out.printf("sumSquaredErrors:\t%f\n", sumSquaredErrors);
+		// displayHiddenShort();
 	}
 
 	private void genHiddenError() {
-		for (Node node : hidden) {
-			double hiddenError = 0.0;
-			for (Connection connection : node.getConnectedTo()) {
-				hiddenError = connection.getToNode().getError() * connection.getWeight() * (1 - node.getValue())
-						* node.getValue();
-				node.setError(hiddenError);
+		for (ArrayList<Node> hiddenLayer : hiddenLayers) {
+			for (Node node : hiddenLayer) {
+				double hiddenError = 0.0;
+				for (Connection connection : node.getConnectedTo()) {
+					hiddenError = connection.getToNode().getError() * connection.getWeight() * (1 - node.getValue())
+							* node.getValue();
+					node.setError(hiddenError);
+				}
+				// System.out.printf("hiddenError: %f\n", hiddenError);
 			}
-			// System.out.printf("hiddenError: %f\n", hiddenError);
+			// System.out.println();
 		}
-		System.out.println();
 	}
 
 	private void updateHiddenWeights() {
-		updateWeight(hidden);
+		for (ArrayList<Node> hiddenLayer : hiddenLayers) {
+			updateWeight(hiddenLayer);
+		}
+		// updateWeight(hidden1);
 	}
 
 	private void genOutputError() {
 		for (Node node : outputs) {
 			double error = 0.0;
-			;
+
 			for (Connection connection : node.getConnectedFrom()) {
 				double output = node.getValue();
 				error = (target - output) * (1 - output) * output;
@@ -181,8 +239,8 @@ public class Runner {
 			}
 		}
 	}
-	
-	private void displayInputsShort(){
+
+	private void displayInputsShort() {
 		System.out.print("Target: " + target + "\t\t");
 		System.out.print("Inputs:\t\t");
 		for (Node node : inputs) {
@@ -192,21 +250,24 @@ public class Runner {
 	}
 
 	private void displayHiddenShort() {
-		System.out.println("*Hidden Values/Weights");
-		for (Node node : hidden) {
-			System.out.print("value:\t" + node.getValue());
-			System.out.print("\tweights_from:\t");
-			for (Connection connection : node.getConnectedFrom()) {
-				System.out.printf("%.5f\t\t", connection.getWeight());
-			}
+		int count = 0;
+		for (ArrayList<Node> hiddenLayer : hiddenLayers) {
+			count++;
+			System.out.println("Hidden Layer: " + count);
+			for (Node node : hiddenLayer) {
+				System.out.print("value:\t" + node.getValue());
+				System.out.print("\tweights_from:\t");
+				for (Connection connection : node.getConnectedFrom()) {
+					System.out.printf("%.5f\t\t", connection.getWeight());
+				}
 
-			System.out.print("\tweights_to:\t");
-			for (Connection connection : node.getConnectedTo()) {
-				System.out.printf("%.5f\t", connection.getWeight());
+				System.out.print("\tweights_to:\t");
+				for (Connection connection : node.getConnectedTo()) {
+					System.out.printf("%.5f\t", connection.getWeight());
+				}
+				System.out.println();
 			}
-			System.out.println();
 		}
-		System.out.println();
 	}
 
 	private void displayAll() {
@@ -234,20 +295,24 @@ public class Runner {
 	}
 
 	private void displayHidden() {
-		// display values
-		for (Node node : hidden) {
-			System.out.println("value:\t\t" + node.getValue());
-			System.out.print("weights from:\t");
-			for (Connection connection : node.getConnectedFrom()) {
-				System.out.print(connection.getWeight() + "\t");
-			}
-			System.out.println();
+		int count = 0;
+		for (ArrayList<Node> hiddenLayer : hiddenLayers) {
+			count++;
+			System.out.println("Hidden Layer " + count);
+			for (Node node : hiddenLayer) {
+				System.out.println("value:\t\t" + node.getValue());
+				System.out.print("weights from:\t");
+				for (Connection connection : node.getConnectedFrom()) {
+					System.out.print(connection.getWeight() + "\t");
+				}
+				System.out.println();
 
-			System.out.print("weights to:\t");
-			for (Connection connection : node.getConnectedTo()) {
-				System.out.print(connection.getWeight() + "\t");
+				System.out.print("weights to:\t");
+				for (Connection connection : node.getConnectedTo()) {
+					System.out.print(connection.getWeight() + "\t");
+				}
+				System.out.println("\n");
 			}
-			System.out.println("\n");
 		}
 	}
 
@@ -266,7 +331,12 @@ public class Runner {
 
 	private void genAllValues() {
 		// System.out.println("*Generating hidden node values");
-		genValues(hidden);
+		for (ArrayList<Node> hiddenLayer : hiddenLayers) {
+			genValues(hiddenLayer);
+		}
+		// genValues(hidden1);
+		//
+		// genValues(hidden2);
 
 		// System.out.println("*Generating output node values");
 		genValues(outputs);
@@ -305,18 +375,27 @@ public class Runner {
 		// connect inputs to hidden
 		Connection tempConnection;
 		for (Node inputNode : inputs) {
-			for (Node hiddenNode : hidden) {
-				tempConnection = new Connection(inputNode, hiddenNode, random.nextFloat());
+			for (Node hiddenNode1 : hidden1) {
+				tempConnection = new Connection(inputNode, hiddenNode1, random.nextFloat());
 				inputNode.addConnectedTo(tempConnection);
-				hiddenNode.addConnectedFrom(tempConnection);
+				hiddenNode1.addConnectedFrom(tempConnection);
+			}
+		}
+
+		// connect hidden1 nodes to hidden2 nodes
+		for (Node hiddenNode1 : hidden1) {
+			for (Node hiddenNode2 : hidden2) {
+				tempConnection = new Connection(hiddenNode1, hiddenNode2, random.nextFloat());
+				hiddenNode1.addConnectedTo(tempConnection);
+				hiddenNode2.addConnectedFrom(tempConnection);
 			}
 		}
 
 		// connect hidden to outputs
-		for (Node hiddenNode : hidden) {
+		for (Node hiddenNode2 : hidden2) {
 			for (Node outputNode : outputs) {
-				tempConnection = new Connection(hiddenNode, outputNode, random.nextFloat());
-				hiddenNode.addConnectedTo(tempConnection);
+				tempConnection = new Connection(hiddenNode2, outputNode, random.nextFloat());
+				hiddenNode2.addConnectedTo(tempConnection);
 				outputNode.addConnectedFrom(tempConnection);
 			}
 		}
@@ -328,9 +407,19 @@ public class Runner {
 			inputs.add(new Node(random.nextFloat()));
 		}
 
-		for (int i = 0; i < hiddenNum; i++) {
-			hidden.add(new Node());
+		for (ArrayList<Node> hiddenLayer : hiddenLayers) {
+			for (int i = 0; i < hiddenNum; i++) {
+				hiddenLayer.add(new Node());
+			}
 		}
+
+		// for (int i = 0; i < hiddenNum; i++) {
+		// hidden1.add(new Node());
+		// }
+		//
+		// for (int i = 0; i < hiddenNum; i++) {
+		// hidden2.add(new Node());
+		// }
 
 		for (int i = 0; i < outputNum; i++) {
 			outputs.add(new Node());
